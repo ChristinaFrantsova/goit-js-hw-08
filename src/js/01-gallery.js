08bb7e38eb7eba1fdded554f3cf12c2ca5b4652a
ChristinaFrantsova <mob0964442737@gmail.com>
2022-10-11 15:44:01 +0300

    add the 1-th task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+// 1.Створення галереї
+const galleryImages = document.querySelector('.gallery');
+
+const itemOfGallery = galleryItems
+  .map(
+    element =>
+      `<a class="gallery__item" href=${element.original}>
+  <img class="gallery__image" src=${element.preview} alt=${element.description} />
+</a>`
+  )
+  .join(' ');
+console.log(itemOfGallery);
+
+galleryImages.insertAdjacentHTML('afterbegin', itemOfGallery);
+
+// Через «Options» - документацію додали
+// відображення підписів до зображень з атрибута та
+// час появлення підпису
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
